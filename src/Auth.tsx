0f444b86fafd0a8e5c229d977333e08fc08cd670
m4nute <mgudinocontacto@gmail.com>
2023-04-13 13:23:47 -0300

    ✨RHF Login Form and Login Function

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,5 +1,26 @@
+import { useState } from 'react'
 import pb from './lib/pocketbase'
+import { useForm } from 'react-hook-form'
 
 export default function Auth() {
-    return <>Logged in: {pb.authStore.isValid.toString()}</>
+
+    const { register, handleSubmit } = useForm()
+
+    const [loading, setLoading] = useState(false)
+
+    async function login(data: any) {
+        setLoading(true)
+        const authData = await pb.collection('users').authWithPassword(data.email, data.password)
+        setLoading(false)
+    }
+
+    return <>
+        Logged in: {pb.authStore.isValid.toString()}
+        <form onSubmit={handleSubmit(login)}>
+            <input type="text" placeholder='email' {...register('email')} />
+            <input type="password" placeholder='password' {...register('password')} />
+            <button type='submit'>Login</button>
+        </form>
+    </>
+
 }
